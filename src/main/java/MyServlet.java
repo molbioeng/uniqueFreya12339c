4bbc4dd10be9701878b597643cd3a59a75b45659
molbioeng <the.freya.morris@gmail.com>
2021-11-19 09:37:58 +0000

    FM - added HTML to response

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -11,7 +11,48 @@ public class MyServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException{
                 resp.setContentType("text/html");
-                resp.getWriter().write("Hello, world!");
+                resp.getWriter().write("<ul class=\"level-one\">\n" +
+                        "<li>\n" +
+                        "<p class=\"first-para\"><b>Headings:</b> </p>\n" +
+                        "<h1>Top heading</h1>\n" +
+                        "<p>, </p>\n" +
+                        "<h2 id=\"tab5\">Next-level heading</h2>\n" +
+                        "<p>, … </p>\n" +
+                        "<h6>Lowest heading</h6>\n" +
+                        "\n" +
+                        "</li>\n" +
+                        "<li>\n" +
+                        "<p class=\"first-para\"><b>Bold:</b> <b>text</b></p>\n" +
+                        "</li>\n" +
+                        "<li>\n" +
+                        "<p class=\"first-para\"><b>Italic:</b> <i>text</i></p>\n" +
+                        "</li>\n" +
+                        "<li>\n" +
+                        "<p class=\"first-para\"><b>Paragraph:</b> </p>\n" +
+                        "<p>A paragraph of text</p>\n" +
+                        "\n" +
+                        "</li>\n" +
+                        "<li>\n" +
+                        "<p class=\"first-para\"><b>Bulleted list:</b> </p>\n" +
+                        "<ul>\n" +
+                        "<li>First list item</li>\n" +
+                        "<li>Second list item</li>\n" +
+                        "</ul>\n" +
+                        "</li>\n" +
+                        "<li>\n" +
+                        "<p class=\"first-para\"><b>Numbered list:</b> </p>\n" +
+                        "<ol>\n" +
+                        "<li>Item #1</li>\n" +
+                        "<li>Item #2</li>\n" +
+                        "</ol>\n" +
+                        "</li>\n" +
+                        "<li>\n" +
+                        "<p class=\"first-para\"><b>Bulleted list:</b> </p>\n" +
+                        "<ul>\n" +
+                        "<li>First list item</li>\n" +
+                        "<li>Second list item</li>\n" +
+                        "</ul>\n" +
+                        "</li></ul>");
                 resp.getWriter().write("\n ServletPath of the URL: " + req.getServletPath());
     }
 
@@ -21,3 +62,5 @@ public class MyServlet extends HttpServlet {
 
     }
 }
+
+
